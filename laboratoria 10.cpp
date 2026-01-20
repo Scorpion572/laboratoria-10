7274836009044dddbb0fc6dc95594c76c3f97673
Scorpion572 <wencel_jakub@wp.pl>
2026-01-20 16:26:00 +0100

    1 zadanie i zaczete 2

--- laboratoria 10.cpp
+++ laboratoria 10.cpp
@@ -1,10 +1,81 @@
-﻿#include <iostream>
+﻿//################################### Lab 10 code ###################################
+
+#include <iostream>
+#include <stdio.h>
 using namespace std;
-int main()
-{
-  
 
+void insertNode(int key, string data);
+void displayList();
 
+struct Node { //kod 10_01
+    int key = -1;
+    string data = "";
+    Node* next = nullptr;
+};
+
+Node* head = nullptr; //globalny wskaznik zawsze pokazujacy pierszy wezel
+Node* lastAded = nullptr;
+void insertNode(int key, string data) {
+    Node* newNode = new Node();
+    newNode->key = key;
+    newNode->data = data;
+    newNode->next = nullptr;
+
+    if (head == nullptr) {
+        head = newNode; //jak nic nie ma, stworzy pierszy wezel
+		lastAded = newNode;
+    }
+    else {
+        lastAded->next = newNode;
+		lastAded = newNode;
+    }
+}
+    
+void displayList() {
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << "Key: " << temp->key << ", Data: " << temp->data << endl;
+        temp = temp->next;
+    }
 }
 
+void wyswielmenu() {
+	cout << "Menu:" << endl;
+	cout << "1. Dodaj nowy element" << endl;
+	cout << "2. Usun element" << endl;
+    cout << "3. Wyswietl liste" << endl;
+	cout << "9. Zakoncz program" << endl;
+}
+
+int main() {
+	bool aktywny = true;
+    int n, key;
+    string data;
+    cout << "Enter the number of nodes: ";
+    cin >> n;
+    while (aktywny==true){
+		system("cls");
+        wyswielmenu();
+		char wybor;
+		cout << "Wybierz opcje: ";
+		cin >> wybor;
+		switch (wybor) {
+        case '1':
+
+    }
+    
+
+  
+    /*for (int i = 0; i < n; i++) {
+        cout << "Enter key and data for node " << i + 1 << ": " << endl;
+        cout << "Please write integer number: ";
+        cin >> key;
+        cout << "Please write any string: ";
+        cin >> data;
+        insertNode(key, data);
+    }
+
+    cout << "The entire list:" << endl;
+    displayList();*/
+}
 
